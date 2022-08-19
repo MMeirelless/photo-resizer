307fc5c20f00447e7180af4413ed66dc403f22c6
Matheus Meirelles <matheus.meirellessilva@hotmail.com>
2022-08-19 16:42:05 -0300

    Final version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-from PIL import Image
+from PIL import Image, ImageSequence
 
 photos_path = r'photos'
+new_size = (1024, 9999) # The height was set as 9999 because we want to preserve the image aspects and quality. So this method auto calculated which is the best Height
 
 for root, dirs, files in os.walk(photos_path):
     for file in files:
@@ -10,7 +11,23 @@ for root, dirs, files in os.walk(photos_path):
         photo_format = file.split(".")[len(file.split("."))-1]
         photo_format = "jpeg" if photo_format == "jpg" else photo_format
 
-        image = Image.open(raw_photo_path)
-        image.thumbnail((1024, 9999), Image.ANTIALIAS) # The height was set as 9999 because we want to preserve the image aspects and quality. So this method auto calculated which is the best Height
-        image.save(new_photo_path, photo_format.upper(), quality=100)
-        image.show()
+        if photo_format != "gif":
+            image = Image.open(raw_photo_path)
+            image.thumbnail(new_size, Image.ANTIALIAS) 
+            image.save(new_photo_path, photo_format.upper(), quality=100)
+
+        else:
+            gif = Image.open(raw_photo_path)
+            frames = ImageSequence.Iterator(gif)
+
+            def thumbnails(frames):
+                for frame in frames:
+                    thumbnail = frame.copy()
+                    thumbnail.thumbnail(new_size, Image.ANTIALIAS)
+                    yield thumbnail
+
+            frames = thumbnails(frames)
+
+            new_gif = next(frames)
+            new_gif.info = gif.info
+            new_gif.save(new_photo_path, save_all=True, append_images=list(frames))
